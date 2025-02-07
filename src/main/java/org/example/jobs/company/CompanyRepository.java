3961af789bef5427e14fecb3800b5909db967e51
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-02-07 15:22:06 +0100

    adds company repository

--- src/main/java/org/example/jobs/company/CompanyRepository.java
+++ src/main/java/org/example/jobs/company/CompanyRepository.java
@@ -1,6 +1,8 @@
-package org.example.jobs;
+package org.example.jobs.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 }
