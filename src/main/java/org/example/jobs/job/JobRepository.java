056276892d2199f8b78282af71f4df2878409b8a
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-02-07 13:14:19 +0100

    completes jobrepository

--- src/main/java/org/example/jobs/job/JobRepository.java
+++ src/main/java/org/example/jobs/job/JobRepository.java
@@ -1,4 +1,8 @@
 package org.example.jobs.job;
 
-public interface JobRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Integer> {
 }
