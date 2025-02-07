40fc83ae207b3340f6ecc59597f17ff3f3b72a5f
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-02-07 15:20:45 +0100

    adds company entity

--- src/main/java/org/example/jobs/company/Company.java
+++ src/main/java/org/example/jobs/company/Company.java
@@ -1,4 +1,37 @@
 package org.example.jobs.company;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public Company() {
+    }
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
