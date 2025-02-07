bef76577a71efaa7d78cbb20237a13ed86c7d215
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-02-07 12:52:45 +0100

    completes job service and implementation

--- src/main/java/org/example/jobs/job/JobService.java
+++ src/main/java/org/example/jobs/job/JobService.java
@@ -1,4 +1,10 @@
 package org.example.jobs.job;
 
+import java.util.List;
+
 public interface JobService {
+    boolean addJob(Job job);
+    List<Job> jobs();
+    boolean updateJob(int id, Job job);
+    boolean deleteJob(int id);
 }
--- src/main/java/org/example/jobs/job/JobServiceImpl.java
+++ src/main/java/org/example/jobs/job/JobServiceImpl.java
@@ -1,4 +1,44 @@
 package org.example.jobs.job;
 
-public class JobServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Override
+    public boolean addJob(Job job) {
+        jobRepository.save(job);
+        return true;
+    }
+
+    @Override
+    public List<Job> jobs() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public boolean updateJob(int id, Job updatedJob) {
+        Job job = jobRepository.findById(id).orElse(null);
+        if (job != null) {
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setMinSalary(updatedJob.getMinSalary());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteJob(int id) {
+        jobRepository.deleteById(id);
+        return true;
+    }
 }
