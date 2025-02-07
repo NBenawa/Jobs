40e1320375a7489ecbe4a7a71d6dee0d1bbd5ff2
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-02-07 15:21:27 +0100

    adds service layer of company

--- src/main/java/org/example/jobs/company/CompanyService.java
+++ src/main/java/org/example/jobs/company/CompanyService.java
@@ -1,4 +1,11 @@
 package org.example.jobs.company;
 
+import java.util.List;
+
 public interface CompanyService {
+    boolean addCompany(Company company);
+    boolean removeCompany(int id);
+    boolean updateCompany(int id, Company company);
+    List<Company> getAllCompanies();
+    Company getCompany(int id);
 }
--- src/main/java/org/example/jobs/company/CompanyServiceImpl.java
+++ src/main/java/org/example/jobs/company/CompanyServiceImpl.java
@@ -1,4 +1,45 @@
 package org.example.jobs.company;
 
-public class CompanyServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Override
+    public boolean addCompany(Company company) {
+        companyRepository.save(company);
+        return true;
+    }
+
+    @Override
+    public boolean removeCompany(int id) {
+        companyRepository.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public boolean updateCompany(int id, Company updatedCompany) {
+        Company company = companyRepository.findById(id).orElse(null);
+        if (company != null) {
+            company.setName(updatedCompany.getName());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public Company getCompany(int id) {
+        return companyRepository.findById(id).orElse(null);
+    }
 }
