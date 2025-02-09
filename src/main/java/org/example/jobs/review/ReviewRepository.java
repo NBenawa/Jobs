8a9d2684d44334735061e5b1a23b87b6d96c9e31
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-02-09 20:22:13 +0100

    adds repository for review

---
+++ src/main/java/org/example/jobs/review/ReviewRepository.java
@@ -0,0 +1,11 @@
+package org.example.jobs.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByCompanyId(int companyId);
+}
