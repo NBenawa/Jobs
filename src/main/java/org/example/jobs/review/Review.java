605581c20c8a3994f04e06bd9e5a7d23cad4829e
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-02-09 20:19:03 +0100

    adds review entity

--- src/main/java/org/example/jobs/review/Review.java
+++ src/main/java/org/example/jobs/review/Review.java
@@ -1,4 +1,69 @@
 package org.example.jobs.review;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.example.jobs.company.Company;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private int rating;
+
+    @ManyToOne
+    private Company company;
+
+    public Review() {
+    }
+
+    public Review(String title, String description, int rating, Company company) {
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+        this.company = company;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
